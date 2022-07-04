5decf46fd2b71fb3fb2734085dd22ae7f4a1ab64
masterkai <kai0919@hotmail.com>
2022-07-05 02:09:22 +0800

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import { deck$, Pokemon, pokemon$, selected$ } from "./store";
+import { useObservableState } from "observable-hooks";
 
-function App() {
+const Deck = () => {
+  const deck = useObservableState(deck$, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>Deck</h4>
+      <div>
+        {deck.map((p) => (
+          <div key={p.id} style={{ display: "flex", alignItems: "center" }}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`}
+              alt={p.name}
+            />
+            <div>
+              <div>{p.name}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
+};
+const Search = () => {
+    const [search, setSearch] = useState("");
+    const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+    useEffect(() => {
+        const subscription = pokemon$.subscribe(setPokemon);
+        return () => subscription.unsubscribe();
+    }, []);
+
+    const filteredPokemon = useMemo(() => {
+        return pokemon.filter((p) =>
+            p.name.toLowerCase().includes(search.toLowerCase())
+        );
+    }, [search, pokemon]);
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <div
+                style={{
+                    maxHeight: "70vh",
+                    overflow: "scroll",
+                }}
+            >
+                {filteredPokemon.map((p) => (
+                    <div key={p.name}>
+                        <input
+                            type="checkbox"
+                            checked={p.selected}
+                            onChange={() => {
+                                console.log(selected$);
+                                if (selected$.value.includes(p.id)) {
+                                    selected$.next(selected$.value.filter((id) => id !== p.id));
+                                } else {
+                                    selected$.next([...selected$.value, p.id]);
+                                }
+                            }}
+                        />
+                        <strong>{p.name}</strong> - {p.power}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+function App() {
+    return (
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns: "1fr 1fr",
+            }}
+        >
+            <Search/>
+            <Deck/>
+        </div>
+    );
 }
 
 export default App;
